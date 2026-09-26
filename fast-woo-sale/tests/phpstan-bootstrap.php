d3d36a319b4da0ef37b70f57ed26f98954fb0ca3
sangemashhad-sys <sangemashhad@gmail.com>
2026-09-26 21:00:17 +0330

    Delete fast-woo-sale directory

--- fast-woo-sale/tests/phpstan-bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Runtime constants declared for PHPStan only.
- */
-define( 'FWS_VERSION', '2.8.0' );
-define( 'FWS_PLUGIN_DIR', __DIR__ . '/../' );
-define( 'FWS_PLUGIN_URL', 'https://example.test/wp-content/plugins/fast-woo-sale/' );
-define( 'FWS_MIN_CONFIDENCE', 60 );
-define( 'FWS_BUNDLE_DISCOUNT', 12 );
